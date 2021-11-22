5d1cc0f278fc9cef0c603ae0435960c43de1bdc3
Elon.Zhang <32865812+ElonZhang-YQ@users.noreply.github.com>
2021-11-22 12:55:19 -0800

    Add files via upload

---
+++ src/test/java/com/ubishops/library/LibraryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ubishops.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
