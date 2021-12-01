e0d60c0517af1e76dd3d07420a02d45407f9d0b6
Elon Zhang <elon.zhangzyq@gmail.com>
2021-12-01 10:44:27 -0800

    Update

--- src/main/java/com/ubishops/library/dao/UserDAOImp.java
+++ src/main/java/com/ubishops/library/dao/UserDAOImp.java
@@ -1,5 +1,6 @@
 package com.ubishops.library.dao;
 
+import com.ubishops.library.entity.User;
 import com.ubishops.library.interfase.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,19 +9,19 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class UserDAOImp implements UserDAO {
+public class UserDAOImp implements UserDAO<User> {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Override
-    public List selectAllUsers() {
+    public List<User> selectAllUsers() {
 //        jdbcTemplate.query();
         return null;
     }
 
     @Override
-    public List selectAllUsersByType(int type) {
+    public List<User> selectAllUsersByType(int type) {
         return null;
     }
 }
